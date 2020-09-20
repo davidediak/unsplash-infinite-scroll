a27f488e273771da8f465b55af3a13ab7eb28015
david <davidediak@gmail.com>
2020-09-20 13:38:36 +0200

    redirect to / if no matching route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 import home from './pages/Home';
 import {MuiThemeProvider} from '@material-ui/core/styles';
@@ -7,6 +7,7 @@ import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './assets/theme';
 import store from './redux/store';
 import {Provider} from 'react-redux';
+import {Redirect} from 'react-router';
 
 const theme = createMuiTheme(themeFile);
 function App() {
@@ -14,8 +15,13 @@ function App() {
     <MuiThemeProvider theme={theme}>
       <Provider store={store}>
         <Router>
-          <Route exact path="/" component={home} />
-          <Route exact path="/search=:queries" component={home} />
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/search=:queries" component={home} />
+            <Route exact path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
         </Router>
       </Provider>
     </MuiThemeProvider>
